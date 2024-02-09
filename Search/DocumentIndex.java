45926916d4986f6a2a53e19a5d141eac0197d040
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-09 13:24:18 +0100

    Update DocumentIndex.java

--- Search/DocumentIndex.java
+++ Search/DocumentIndex.java
@@ -1,1 +1,19 @@
-package de.telran.Ex4_02_09.Search;import java.util.HashMap;import java.util.Map;public class DocumentIndex {    private long lastId = 0;    private final Map<DocumentId, Document> storage = new HashMap<>();    public Document getDocument(DocumentId documentId) {        return storage.get(documentId);    }    public DocumentId save(Document document) {        DocumentId id = new DocumentId(lastId++);        storage.put(id, document);        return id;    }}
+package de.telran.Ex4_02_09.Search;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DocumentIndex {
+    private long lastId = 0;
+    private final Map<DocumentId, Document> storage = new HashMap<>();
+
+    public Document getDocument(DocumentId documentId) {
+        return storage.get(documentId);
+    }
+
+    public DocumentId save(Document document) {
+        DocumentId id = new DocumentId(lastId++);
+        storage.put(id, document);
+        return id;
+    }
+}
