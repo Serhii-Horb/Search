e35448a2d3b7609ebed948c9852ad338d058268b
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-09 13:23:20 +0100

    Update Document.java

--- Search/Document.java
+++ Search/Document.java
@@ -1,1 +1,20 @@
-package de.telran.Ex4_02_09.Search;public class Document {    private final String text;    public Document(String text) {        this.text = text;    }    public String getText() {        return text;    }    @Override    public String toString() {        return "Document{" +                "text='" + text + '\'' +                '}';    }}
+package de.telran.Ex4_02_09.Search;
+
+public class Document {
+    private final String text;
+
+    public Document(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "text='" + text + '\'' +
+                '}';
+    }
+}
