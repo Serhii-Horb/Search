f8a0bec423d381400c4ba0411e688e12e4b7c96b
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-09 13:25:12 +0100

    Update TextSplitter.java

--- Search/TextSplitter.java
+++ Search/TextSplitter.java
@@ -1,1 +1,16 @@
-package de.telran.Ex4_02_09.Search;import java.util.ArrayList;import java.util.List;import java.util.Locale;public class TextSplitter {    public List<Word> splitText(String text) {        List<Word> result = new ArrayList<>();        String[] words = text.split(" ");        for (String word : words) {            result.add(new Word(word.toLowerCase(Locale.ROOT))); // Locale.ROOT передаёт язык который стоит на компе        }        return result;    }}
+package de.telran.Ex4_02_09.Search;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class TextSplitter {
+    public List<Word> splitText(String text) {
+        List<Word> result = new ArrayList<>();
+        String[] words = text.split(" ");
+        for (String word : words) {
+            result.add(new Word(word.toLowerCase(Locale.ROOT))); // Locale.ROOT передаёт язык который стоит на компе
+        }
+        return result;
+    }
+}
