72deb7e73b8c024f4bd3d9b0933ea7fcbf393e1a
Serhii Horb <serhii.horb.ua@gmail.com>
2024-02-09 13:20:00 +0100

    Search 9.02.2024

---
+++ Search/Document.java
@@ -0,0 +1,1 @@
+package de.telran.Ex4_02_09.Search;public class Document {    private final String text;    public Document(String text) {        this.text = text;    }    public String getText() {        return text;    }    @Override    public String toString() {        return "Document{" +                "text='" + text + '\'' +                '}';    }}
---
+++ Search/SearchQuery.java
@@ -0,0 +1,1 @@
+package de.telran.Ex4_02_09.Search;public class SearchQuery {    private final String query;    public SearchQuery(String query) {        this.query = query;    }    public String getQuery() {        return query;    }    @Override    public String toString() {        return "SearchQuery{" +                "query='" + query + '\'' +                '}';    }}
