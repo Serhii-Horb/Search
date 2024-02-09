4bb155465eb58dc75429cebb3e641df44be2b450
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-09 13:24:59 +0100

    Update SearchQuery.java

--- Search/SearchQuery.java
+++ Search/SearchQuery.java
@@ -1,1 +1,20 @@
-package de.telran.Ex4_02_09.Search;public class SearchQuery {    private final String query;    public SearchQuery(String query) {        this.query = query;    }    public String getQuery() {        return query;    }    @Override    public String toString() {        return "SearchQuery{" +                "query='" + query + '\'' +                '}';    }}
+package de.telran.Ex4_02_09.Search;
+
+public class SearchQuery {
+    private final String query;
+
+    public SearchQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchQuery{" +
+                "query='" + query + '\'' +
+                '}';
+    }
+}
