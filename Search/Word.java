d31eae6729c695538676fa87ed1a15a9073617e5
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-09 13:25:23 +0100

    Update Word.java

--- Search/Word.java
+++ Search/Word.java
@@ -1,1 +1,35 @@
-package de.telran.Ex4_02_09.Search;import java.util.Objects;public class Word {    private final String word;    public Word(String word) {        this.word = word;    }    public String getWord() {        return word;    }    @Override    public String toString() {        return "Word{" +                "word='" + word + '\'' +                '}';    }    @Override    public boolean equals(Object o) {        if (this == o) return true;        if (o == null || getClass() != o.getClass()) return false;        Word word1 = (Word) o;        return Objects.equals(word, word1.word);    }    @Override    public int hashCode() {        return Objects.hash(word);    }}
+package de.telran.Ex4_02_09.Search;
+
+import java.util.Objects;
+
+public class Word {
+    private final String word;
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "word='" + word + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return Objects.equals(word, word1.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word);
+    }
+}
