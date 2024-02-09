2b4e838e33608963217127f3a39089672408ac79
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-09 13:24:34 +0100

    Update InvertIndex.java

--- Search/InvertIndex.java
+++ Search/InvertIndex.java
@@ -1,1 +1,30 @@
-package de.telran.Ex4_02_09.Search;import java.util.ArrayList;import java.util.HashMap;import java.util.List;import java.util.Map;public class InvertIndex {    private final Map<Word, List<DocumentId>> storage = new HashMap<>();    public List<DocumentId> getDocumentIds(Word word) {        List<DocumentId> documentsIds = storage.get(word);        if (documentsIds == null) {            return List.of();        }        return documentsIds;    }    public void save(DocumentId documentId, List<Word> words) {        for (Word word : words) {            List<DocumentId> documentIds = storage.get(word);            if (documentIds == null) {                documentIds = new ArrayList<>();            }            documentIds.add(documentId);            storage.put(word, documentIds);        }    }}
+package de.telran.Ex4_02_09.Search;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InvertIndex {
+    private final Map<Word, List<DocumentId>> storage = new HashMap<>();
+
+
+    public List<DocumentId> getDocumentIds(Word word) {
+        List<DocumentId> documentsIds = storage.get(word);
+        if (documentsIds == null) {
+            return List.of();
+        }
+        return documentsIds;
+    }
+
+    public void save(DocumentId documentId, List<Word> words) {
+        for (Word word : words) {
+            List<DocumentId> documentIds = storage.get(word);
+            if (documentIds == null) {
+                documentIds = new ArrayList<>();
+            }
+            documentIds.add(documentId);
+            storage.put(word, documentIds);
+        }
+    }
+}
